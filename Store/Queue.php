11b72b64a642c3a94bdc4feb3cb0502a6d1b2d95
batmanliu9943 <iambatman19940222@163.com>
2018-09-18 18:59:32 +0800

    更新

--- Store/Queue.php
+++ Store/Queue.php
@@ -3,24 +3,93 @@ namespace Core\Store;
 
 use Core\Config;
 
+/**
+ * 消息队列
+ */
 class Queue{
 
-    private $connection;
+    /* 连接 */
+    private static $connection;
+    private static $connect;
+
+    /* 频道 */
+    private static $channel = [];
+
+    /* 交换机 */
+    private static $exchange = [];
+
+    /* 队列 */
+    private static $queue = [];
 
     public function __construct()
     {
         $queueConfig = Config::getConfigByKey('QUEUE_SETTING');
         if(class_exists('AMQPConnection')){
-            $this->connection = new \AMQPConnection($queueConfig);
+            self::$connection = new \AMQPConnection($queueConfig);
+            self::$connect = self::$connection->connect();
+        }else{
+            echo '没有安装AMQP拓展';
+        }
+    }
 
-            $connect = $this->connection->connect();
-            if($connect){
+    public function createQueue($queueName)
+    {
+        if(self::$queue[$qu]){
 
-            }else{
+        }
+    }
 
-            }
+    public function getExchange($exName, $channel)
+    {
+        if(isset(self::$exchange[$exName]) && self::$exchange[$exName]){
+            return self::$exchange[$exName];
         }
 
+        $exchange = new \AMQPExchange($channel);
+        $exchange->setName($exName); 
+        $exchange->setType(AMQP_EX_TYPE_DIRECT);  
+        $exchange->setFlags(AMQP_DURABLE);
+
+        self::$exchange[$exName] = $exchange;
+        return $exchange;
+    }
+
+    public function getExchangeStatus($exchange)
+    {
+        return $exchange->declare();
+    }
+
+    public function getChannel($channelName)
+    {
+        if(isset(self::$channel[$channelName]) && self::$channel[$channelName]){
+            return self::$channel[$channelName];
+        }
+
+        /* 创建新频道 */
+        $channel = new \AMQPChannel(self::$connection);
+        self::$channel[$channelName] = $channel;
+        return $channel;
+    }
+
+    public function getQueue($queueName)
+    {
+        if(isset(self::$queue[$queueName]) && self::$queue[$queueName]){
+            return self::$queue[$queueName];
+        }
+
+        /* 创建新队列 */
+        $queue = new \AMQPQueue($channel);
+        $queue->setName($queueName);
+        /* 持久 */
+        $queue->setFlags(AMQP_DURABLE);
+
+        self::$queue[$queueName] = $queue;
+        return $queue;
+    }
+
+    public function consumer($queue, $exName, $routeKey)
+    {
+        $queue->bind($exName, $routeKey);
     }
 
     public function getCan(){
--- Tool/Page.php
+++ Tool/Page.php
@@ -15,9 +15,11 @@ class Page{
 
 	private $lastPageNumber = 0;
 
-	private $realPageSize = 0;
+	private $realPageSize = [];
 
 	private $requestParam = [];
+
+	private $limitPageNumber = 4;
 
 	public function __construct($currentPageNumber = 1, $pageSizeNumber = 10, $dataCountNumber = 0, $param = [])
 	{
@@ -27,7 +29,48 @@ class Page{
 		$this->lastPageNumber = $this->currentPageNumber <= 1 ? 1 : $this->currentPageNumber - 1;
 		$countPageNumber = ceil($this->dataCountNumber / $this->pageSizeNumber);
 		$this->nextPageNumber = $countPageNumber < $currentPageNumber + 1 ? $currentPageNumber : $currentPageNumber + 1;
-		$this->realPageSize = $countPageNumber;
+
+		
+		
+		if($countPageNumber > $this->limitPageNumber){
+			$size = $this->limitPageNumber - 3;dump($size);
+			if($size % 2 == 0){
+				$frontLength = $backLength = $size / 2;
+			}else{
+				$frontLength = ($size + 1) / 2 ;
+				$backLength = ($size - 1) / 2;
+			}
+// dump($frontLength);dump($backLength);
+			array_push($this->realPageSize, 1);
+
+			$start = $this->currentPageNumber - $frontLength > 1 ? $this->currentPageNumber - $frontLength : 2;
+			$end = $this->currentPageNumber + $backLength > $countPageNumber -1  ? $this->currentPageNumber + $backLength - 1 : $this->limitPageNumber - 2;
+
+			dump($start);dump($end);
+
+			for($i = $start; $i <= $end; $i++ ){
+				array_push($this->realPageSize, $i);
+				if($start == $end){
+					if($start == 2){
+						array_push($this->realPageSize, $countPageNumber - 1);
+					}else{
+						array_push($this->realPageSize, 2);
+					}
+				}
+			}
+
+			array_push($this->realPageSize, $countPageNumber);
+		}else{
+			$start = 1;
+			$end = $countPageNumber;
+			for($i = $start; $i <= $end; $i++ ){
+				array_push($this->realPageSize, $i);
+			}
+		}
+		
+
+		
+
 		if($param){
 			$this->requestParam = $param;
 		}
@@ -36,10 +79,13 @@ class Page{
 	public function getPageHtml()
 	{
 		$html = '';
-		for($page = 1 ; $page <= $this->realPageSize ; $page++){
+		foreach ($this->realPageSize as $page) {
 			$pageNumber = ['currentPage' => $page];
 			$html .= '<li'.($page == $this->currentPageNumber ? ' class="active"' : '').'><a href="'.Router::url('', $pageNumber).'">'.$page.'</a></li>';
 		}
+		// for($page = 1 ; $page <= $this->realPageSize ; $page++){
+			
+		// }
 		$html = '<div class="text-center"><ul class="pagination"><li><a href="'.Router::url('', ['currentPage'=>$this->lastPageNumber]).'">«</a></li>' .$html. '<li><a href="'.Router::url('', ['currentPage'=>$this->nextPageNumber]).'">»</a></li></ul></div>';
 		return $html;
 	}
